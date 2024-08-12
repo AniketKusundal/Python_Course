6dab9c9f5af4f1a0d4e9494dab33921f8fc9b19b
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2024-08-13 01:53:36 +0530

    Add files via upload

---
+++ Practice code/Comments_Escape_Squance_And_Print_Statement.py
@@ -0,0 +1,25 @@
+# **Comments Escape Squance And Print Statement**
+
+# Escape Sequance 
+
+# 1 ] \n ---->  For new Line 
+
+# print("Hello aniket Your are Learning Python \n And you are learning Comments Escape Squance And Print Statement in python")
+
+# #  Comments in Python 
+#     [1] #          --> single line comments 
+#     [2] ''' '''   --> Multi line commnets 
+
+#  Print Statment
+    # The Syntax of the Print Statment
+
+        # print(object(s) ,sup="seprator" , end=end , file=file)
+
+
+# print("Hey" , 6 , 7 ,  sep="~")   # ~ Tilt Opratpor  --> in one print statment multiple value is store then we will use seprator
+
+
+# [2]  end=end
+print("Hey" , 6 , 7 , sep="~" , end="009\n")
+print("Aniket")
+    # ---->>  end specify that the what print at the end 
---
+++ Practice code/Variables_And_Data_Types.py
@@ -0,0 +1,15 @@
+#   Variables And Data Types In Python
+
+a = 1
+b = True
+c = "Aniket"
+d = None
+E = complex(8,3)    # complex is a Numeric data type int , float , complex 
+
+# To check  Data Type Typeof() Function Used
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(E))
